35c3ffd85710da9d9a2af3c9f7218624671bfa58
kadi-hub3 <kicktheball1@gmail.com>
2024-10-08 19:12:16 +0200

    Context creation

---
+++ context/GlobalProvider.jsx
@@ -0,0 +1,26 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+
+const GlobalContext = createContext()
+export const useGlobalContext = () => useContext(GlobalContext)
+
+
+const GlobalProvider = ({children}) => {
+    const [isLogged, setIsLogged] = useState(false)
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(false)
+  return (
+    <GlobalContext.Provider
+        value={{
+            isLogged,
+            setIsLogged,
+            user,
+            setUser,
+            loading
+        }}
+    >
+        {children}
+    </GlobalContext.Provider>
+  )
+}
+
+export default GlobalProvider
