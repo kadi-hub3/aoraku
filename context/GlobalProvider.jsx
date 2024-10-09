ba82b6b96352755e7fadb68367877d4597507ecf
kadi-hub3 <kicktheball1@gmail.com>
2024-10-09 15:44:56 +0200

    Get user from DB

--- context/GlobalProvider.jsx
+++ context/GlobalProvider.jsx
@@ -1,4 +1,5 @@
-import React, { createContext, useContext, useEffect, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react"
+import { getCurrentUser } from "../lib/appwrite";
 
 const GlobalContext = createContext()
 export const useGlobalContext = () => useContext(GlobalContext)
@@ -8,6 +9,26 @@ const GlobalProvider = ({children}) => {
     const [isLogged, setIsLogged] = useState(false)
     const [user, setUser] = useState(null)
     const [loading, setLoading] = useState(false)
+
+    useEffect(()=>{
+        getCurrentUser()
+        .then((res) => {
+          if (res) {
+            setIsLogged(true);
+            setUser(res);
+          } else {
+            setIsLogged(false);
+            setUser(null);
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    })
+
   return (
     <GlobalContext.Provider
         value={{
