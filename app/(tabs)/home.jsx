804e5b54cde383cc7f31d93d4f8d1931b1135508
kadi-hub3 <kicktheball1@gmail.com>
2024-10-10 17:06:14 +0200

    Setting up home page

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,11 +1,57 @@
 import { View, Text } from 'react-native'
-import React from 'react'
-
+import React, { useState } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { FlatList } from 'react-native-gesture-handler'
+import {images} from '../../constants'
 const Home = () => {
+  const [data, setData] = useState([])
+  const [refreshing, setRefreshing] = useState(false)
+  const onRefresh = () => {
+    setRefreshing(true)
+    //await refetch()
+    setRefreshing(false)
+
+  }
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView>
+      <FlatList
+        data={data}
+        keyExtractor={(item)=>item.$id}
+        renderItem={({item})=> (
+          // <VideoCard 
+          // />
+          item
+        )}
+        ListHeaderComponent={()=>(
+          <View className='flex my-6 px-4'>
+            <View className='flex justify-center items-center'>
+              <View>
+                <Text className='font-pmedium text-gray-100'>
+                  Welcome Back
+                </Text>
+                <Text className='font-psemibold text-white'>
+                  Kadi
+                </Text>
+              </View>
+              <View className=''>
+                <Image
+                  source={images.logoSmall}
+                  className='w-9 h-9'
+                  resizeMode='contain'
+                />
+              </View>
+            </View>
+            <SearchInput />
+            <View className='w-full flex-1'>
+              <Text className='text-lg font-pregular text-gray-100'>
+                Latest Videos
+              </Text>
+              <Trending posts={data} />
+            </View>
+          </View>
+        )}
+      />
+    </SafeAreaView>
   )
 }
 
