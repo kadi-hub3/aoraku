9363c422ac81800dd599b3c6263c852459664b03
kadi-hub3 <kicktheball1@gmail.com>
2024-10-14 17:21:38 +0200

    Trending Card

--- components/Trending.jsx
+++ components/Trending.jsx
@@ -28,7 +28,37 @@ const TrendingItem = ({activeItem, item}) => {
             animation={activeItem === item.$id? zoomIn : zoomOut}
             duration={500}
         >
-
+        {play ? (
+            <Video
+                source={{uri: item.video}}
+                className='w-full h-60 rounded-xl'
+                resizeMode={ResizeMode.CONTAIN}
+                useNtiveControls
+                shouldPlay
+                onPlaybackStatusUpdate={(status)=> {
+                    if (status.didJustFinish) {
+                        setPlay(false)
+                    }
+                }}
+            />
+        ) : (
+            <TouchableOpacity
+                activeOpacity={0.7}
+                onPress={()=>setPlay(true)}
+                className='w-full h-60 rounded-xl flex justify-center items-center'
+            >
+                <Image 
+                    source={{uri: item.thumbnail}}
+                    className='w-full h-full rounded-xl'
+                    resizeMode='contain'
+                />
+                <Image
+                    source={icons.play}
+                    className='w-12 h-12 absolute'
+                    resizeMode='contain'
+                />
+            </TouchableOpacity>
+        )}
         </Animatable.View>
     )
 
