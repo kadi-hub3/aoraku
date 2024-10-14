68a97049dd883996bdb3a8eefc8d7a3989390deb
kadi-hub3 <kicktheball1@gmail.com>
2024-10-14 16:57:13 +0200

    VideoCard

--- components/VideoCard.jsx
+++ components/VideoCard.jsx
@@ -1,7 +1,11 @@
 import { View, Text, Image } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
+import { ResizeMode, Video } from "expo-av";
+import { TouchableOpacity } from 'react-native';
+
 
 const VideoCard = ({title, creator, avatar, thumbnail, video}) => {
+    const [play, setPlay] = useState(false)
   return (
     <View className='flex flex-col items-center px-4'>
         <View className='flex flex-row gap-3 items-start'>
@@ -14,16 +18,36 @@ const VideoCard = ({title, creator, avatar, thumbnail, video}) => {
                     />
                 </View>
                 <View className='flex justify-center flex-1'>
-                    <Text className='font-psemibold text-sm text-white'>
+                    <Text className='font-psemibold text-sm text-white' numberOfLines={1}>
                         {title}
                     </Text>
-                    <Text className='text-xs text-gray-200 font-pregular'>
+                    <Text className='text-xs text-gray-200 font-pregular' numberOfLines={1}>
                         {creator}
                     </Text> 
                 </View>
             </View>
-            
+            <View className='pt-2'>
+                <Image source={icons.menu} className='w-5 h-5' resizeMode='contain' />
+            </View>
         </View>
+        {play ? (
+            <Video
+                source={{uri: video}}
+                className='w-full h-60 rounded-xl'
+                resizeMode={ResizeMode.CONTAIN}
+                useNtiveControls
+                shouldPlay
+                onPlaybackStatusUpdate={(status)=> {
+                    if (status.didJustFinish) {
+                        setPlay(false)
+                    }
+                }}
+            />
+        ) : (
+            <TouchableOpacity>
+                
+            </TouchableOpacity>
+        )}
     </View>
   )
 }
