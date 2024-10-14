76e08c0d9826b1cdacf818a54782c649ea8b8755
kadi-hub3 <kicktheball1@gmail.com>
2024-10-14 16:49:13 +0200

    VideoCard

--- components/VideoCard.jsx
+++ components/VideoCard.jsx
@@ -1,10 +1,29 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
 
-const VideoCard = () => {
+const VideoCard = ({title, creator, avatar, thumbnail, video}) => {
   return (
-    <View>
-      <Text>VideoCard</Text>
+    <View className='flex flex-col items-center px-4'>
+        <View className='flex flex-row gap-3 items-start'>
+            <View className='flex flex-row justify-center items-center'>
+                <View className='w-[46px] h-[46px] rounded-lg border border-secondary flex justify-center items-center'>
+                    <Image
+                        source={{uri: avatar}}
+                        className='w-full h-fu;; rounded-lg'
+                        resizeMode='contain'
+                    />
+                </View>
+                <View className='flex justify-center flex-1'>
+                    <Text className='font-psemibold text-sm text-white'>
+                        {title}
+                    </Text>
+                    <Text className='text-xs text-gray-200 font-pregular'>
+                        {creator}
+                    </Text> 
+                </View>
+            </View>
+            
+        </View>
     </View>
   )
 }
