e4bb40b480ca28514b28923e7ef44cab45c67b83
kadi-hub3 <kicktheball1@gmail.com>
2024-11-04 18:50:10 +0100

    Search Query compoennt

--- components/SearchField.jsx
+++ components/SearchField.jsx
@@ -1,35 +1,39 @@
-import { View, Text, TextInput, TouchableOpacity, Image } from 'react-native'
-import React from 'react'
-import { icons } from '../constants'
+import { useState } from "react";
+import { router, usePathname } from "expo-router";
+import { View, TouchableOpacity, Image, TextInput, Alert } from "react-native";
 
-const SearchField = () => {
+import { icons } from "../constants";
+
+const SearchField = ({ initialQuery }) => {
+  const pathname = usePathname();
+  const [query, setQuery] = useState(initialQuery || "");
+
   return (
-    <View className='space-y-2'>
-      <Text className={`text-gray-100 font-pmedium ${otherStyles}`}>
-        {title}
-      </Text>
-      <View className='border-black-200 h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary-100 flex flex-row items-center'>
-        <TextInput  
-            className='flex-1 text-white font-psemibold text-base'
-            value={value}
-            placeholder={placeholder}
-            placeholderTextColor="gray"
-            onChangeText={handleChange}
-            secureTextEntry={title==='Password' && !showPassword}
-        />
-     
-          <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
-            <Image
-              source={icons.search}
-              className="w-6 h-6"
-              resizeMode="contain"
-            />
-          </TouchableOpacity>
-      
+    <View className="flex flex-row items-center space-x-4 w-full h-16 px-4 bg-black-100 rounded-2xl border-2 border-black-200 focus:border-secondary">
+      <TextInput
+        className="text-base mt-0.5 text-white flex-1 font-pregular"
+        value={query}
+        placeholder="Search a video topic"
+        placeholderTextColor="#CDCDE0"
+        onChangeText={(e) => setQuery(e)}
+      />
 
-      </View>
+      <TouchableOpacity
+        onPress={() => {
+          if (query === "")
+            return Alert.alert(
+              "Missing Query",
+              "Please input something to search results across database"
+            );
+
+          if (pathname.startsWith("/search")) router.setParams({ query });
+          else router.push(`/search/${query}`);
+        }}
+      >
+        <Image source={icons.search} className="w-5 h-5" resizeMode="contain" />
+      </TouchableOpacity>
     </View>
-  )
-}
+  );
+};
 
-export default SearchField
+export default SearchField;
